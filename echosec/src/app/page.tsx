d348e775519eb7558639a76d4c264361bbe807ec
Samuel Lucas de Almeida <samuel.almeida@sou.inteli.edu.br>
2023-08-13 17:37:27 -0300

    Adicionando scrolldown

--- echosec/src/app/page.tsx
+++ echosec/src/app/page.tsx
@@ -10,6 +10,7 @@ import githubLogo from "@assets/logo_github.svg";
 import { CardPhase } from '@/components/CardPhase';
 import { AboutUs } from '@/components/AboutUs';
 import Link from 'next/link';
+import ScrollButton from '@/components/ScrollButton';
 
 
 const urls = {
@@ -24,7 +25,7 @@ export default function Home() {
     <Layout>
       <div className="pt-24 min-h-screen h-auto w-full flex items-center justify-center flex-col">
         <div className="flex flex-col w-full justify-center items-center mb-6 -mt-1 max-md:mt-6 max-xl:mt-1 max-[760px]:mb-80">
-          <div className="mt-10 w-52 h-20 mb-10 flex justify-center items-center max-sm:mt-4">
+          <div className="mt-10 w-52 h-20 mb-5 flex justify-center items-center max-sm:mt-4">
             <h1 className="font-poppins font-bold text-white text-5xl">
               FALTAM
             </h1>
@@ -41,19 +42,7 @@ export default function Home() {
           </div>
           <div className="flex justify-center items-center flex-col gap-2 max-sm:mb-2 max-md:h-max">
             <p className="font-montserrat font-medium text-sm text-white text-center">Saiba Mais</p>
-            <div className="w-6 h-8 text-white flex items-center justify-center cursor-default">
-              <svg
-                stroke="currentColor"
-                fill="currentColor"
-                stroke-width="1em"
-                viewBox="0 0 1024 1024"
-                height="3em"
-                width="1.5em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M862 465.3h-81c-4.6 0-9 2-12.1 5.5L550 723.1V160c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v563.1L255.1 470.8c-3-3.5-7.4-5.5-12.1-5.5h-81c-6.8 0-10.5 8.1-6 13.2L487.9 861a31.96 31.96 0 0 0 48.3 0L868 478.5c4.5-5.2.8-13.2-6-13.2z"></path>
-              </svg>
-            </div>
+            <ScrollButton />
           </div>
         </div>
         <div className="flex justify-center items-center flex-col pb-4 mb-10 max-sm:h-max max-xl:h-max">
